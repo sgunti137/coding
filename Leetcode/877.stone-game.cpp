80ad5cecbd2440498a2eb42c80087c9e49de2c21
sgunti137 <saaketh.gunti2002@gmail.com>
2026-08-02 18:38:08 +0530

    LC 877

---
+++ Leetcode/877.stone-game.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=877 lang=cpp
+ *
+ * [877] Stone Game
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+// @lc code=end
+
