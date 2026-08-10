1fee0be1765186fa6c28e9c4c1267cf85a8bfca9
sgunti137 <saaketh.gunti2002@gmail.com>
2026-08-10 17:24:27 +0530

    LC 3348 Unsolved

--- Leetcode/3348.smallest-divisible-digit-product-ii.cpp
+++ Leetcode/3348.smallest-divisible-digit-product-ii.cpp
@@ -1,1 +1,30 @@
 // @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+  string smallestNumber(string num, long long t) {
+    vector<int> p = {2, 3, 5, 7};
+    vector<int> m(4, 0);
+
+    for (int i = 0; i < 4; ++i) {
+      while (t != 1 && t % p[i] == 0) {
+        m[i]++;
+        t /= p[i];
+      }
+    }
+
+    if (t > 1) {
+      return "-1";
+    }
+
+    for (int i = 0; i < num.size(); ++i) {
+    }
+
+    return "1";
+  }
+};
+// @leet end
