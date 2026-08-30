99d3760ef6bd4c336b651fefe8b42c1cc2a47a89
sgunti137 <saaketh.gunti2002@gmail.com>
2026-08-29 22:36:06 -0400

    LC 2091

--- Leetcode/2091.removing-minimum-and-maximum-from-array.cpp
+++ Leetcode/2091.removing-minimum-and-maximum-from-array.cpp
@@ -6,8 +6,26 @@ using namespace std;
 // @leet start
 class Solution {
 public:
-    int minimumDeletions(vector<int>& nums) {
-        
+  int minimumDeletions(vector<int> &nums) {
+    int n = nums.size();
+
+    int mi = INT_MAX, ma = INT_MIN;
+    int a = -1, b = -1;
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] > ma) {
+        ma = nums[i];
+        a = i;
+      }
+      if (nums[i] < mi) {
+        mi = nums[i];
+        b = i;
+      }
     }
+
+    if (a > b)
+      swap(a, b);
+
+    return min({b + 1, n - a, a + n - b + 1});
+  }
 };
 // @leet end
