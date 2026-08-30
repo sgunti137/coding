11f75a2112a0aa0b4d3ac4d45cd54654ce7f6208
sgunti137 <saaketh.gunti2002@gmail.com>
2026-08-29 22:35:34 -0400

    LC 2091

---
+++ Leetcode/2091.removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,13 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        
+    }
+};
+// @leet end
