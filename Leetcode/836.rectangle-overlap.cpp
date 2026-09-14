e89a9796abe8ecfb8240def7d0c29bd9074530d6
sgunti137 <saaketh.gunti2002@gmail.com>
2026-09-14 05:21:08 -0400

    LC 836

---
+++ Leetcode/836.rectangle-overlap.cpp
@@ -0,0 +1,13 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        
+    }
+};
+// @leet end
