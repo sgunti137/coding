4531b0f1f36ff8568a413ea9c996e75b2d10f817
sgunti137 <saaketh.gunti2002@gmail.com>
2026-09-15 12:37:21 -0400

    Updated the headers from bits/stdc to generic

--- AdventOfCode/Day3.cpp
+++ AdventOfCode/Day3.cpp
@@ -1,4 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <utility>
+#include <tuple>
+#include <iterator>
+#include <string>
+#include <limits>
+#include <complex>
+#include <random>
+#include <chrono>
 using namespace std;
 #define sim template < class c
 #define ris return * this
