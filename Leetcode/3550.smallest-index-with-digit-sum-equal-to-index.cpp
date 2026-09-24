550de104f62686272c4b496a1a843f3c2ab9e170
sgunti137 <saaketh.gunti2002@gmail.com>
2026-09-24 15:24:35 -0400

    LC 3550

--- Leetcode/3550.smallest-index-with-digit-sum-equal-to-index.cpp
+++ Leetcode/3550.smallest-index-with-digit-sum-equal-to-index.cpp
@@ -7,7 +7,19 @@ using namespace std;
 class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
-        
+		
+		for (int i = 0; i < nums.size(); ++i) {
+			int t = nums[i];
+			int sum = 0;
+			while (t > 0) {
+				sum += t % 10;
+				t /= 10;
+			}
+
+			if (sum == i) return i;
+		}
+
+		return -1;
     }
 };
 // @leet end
